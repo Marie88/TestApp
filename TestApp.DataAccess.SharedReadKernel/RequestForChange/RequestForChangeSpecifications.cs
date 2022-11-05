4a764bc404c5d45ac9e424720fd81ab77f74f456
Maria.Fatu <maria.fatu@sita.aero>
2022-11-05 08:30:12 +0100

    Add project files.

---
+++ TestApp.DataAccess.SharedReadKernel/RequestForChange/RequestForChangeSpecifications.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace IoCore.SharedReadKernel.RequestForChange
+{
+    internal static class RequestForChangeSpecifications
+    {
+        public static IQueryable<T> That<T>(this IQueryable<T> queryable) where T : class
+        {
+            return queryable;
+        }
+
+        public static IQueryable<RequestForChange> AreInProgress(this IQueryable<RequestForChange> requestsForChange)
+        {
+            return requestsForChange.Where(requestForChange => requestForChange.Status == "INPROGRESS");
+        }
+
+        public static IQueryable<RequestForChange> AreAgainstDeprecatedProducts(this IQueryable<RequestForChange> requestsForChange, DateTime since)
+        {
+            return requestsForChange.Where(requestForChange => requestForChange.DateRaised > since);
+        }
+
+        public static IQueryable<RequestForChange> AreWithDrawn(this IQueryable<RequestForChange> requestsForChange)
+        {
+            return requestsForChange.Where(requestForChange => requestForChange.Status == "WITHDRAWN");
+        }
+
+        public static IQueryable<RequestForChange> AreCompleted(this IQueryable<RequestForChange> requestsForChange)
+        {
+            return requestsForChange.Where(requestForChange => requestForChange.Status == "COMPLETED");
+        }
+    }
+}
